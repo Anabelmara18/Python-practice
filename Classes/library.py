145caae7a9c5c4fb5fed37d8c5dd5902a2116a20
Anabelmara18 <amarachiamankwe@gmail.com>
2025-09-14 00:16:45 +0100

    Personal Practice

---
+++ Classes/library.py
@@ -0,0 +1,23 @@
+class books:
+
+    def __init__(self, query):
+        self.query = query
+    
+    def title(self):
+        print("The Alchemy")
+
+    def author(self):
+        print("Paul Coelho")
+
+    def copies(self):
+        print("3")
+
+class library(books):
+    
+book = books({title}, {author}, {copies})
+
+
+
+        
+
+ 
